d713bbc359b9c316068b8be6b0b6d03c81bcd926
Yoshi Yamaguchi <yoshifumi@google.com>
2019-02-06 16:43:03 +0900

    tokenize: rough implementation

--- src/tokenize/main.go
+++ src/tokenize/main.go
@@ -15,5 +15,161 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+
 	"google.golang.org/grpc"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	pb "github.com/ymotongpoo/stackdriver-error-reporting-demo/src/tokenize/genproto"
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+const (
+	listenPort       = 5050
+	projectID        = "yoshifumi-cloud-demo"
+	traceLogFieldKey = "logging.googleapis.com/trace"
+	spanLogFieldKey  = "logging.googleapis.com/span"
+
+	grpcTimeout = 3 * time.Second
+)
+
+var (
+	logger   *zap.SugaredLogger
+	exporter *stackdriver.Exporter
+
+	countSvcAddr string = "countservice:5051"
+)
+
+func main() {
+	var err error
+	ctx := context.Background()
+
+	logger, err = initLogger()
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+	go initTraceAndStats()
+
+	ts, err := NewTokenizeServiceServer(ctx, countSvcAddr)
+	if err != nil {
+		logger.Fatalf("failed to create gRPC server: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", listenPort))
+	if err != nil {
+		logger.Fatalf("failed to listen port %v: %v", listenPort, err)
+	}
+	srv := grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
+	pb.RegisterTokenizeServiceServer(srv, ts)
+	err = srv.Serve(lis)
+	logger.Fatal(err)
+}
+
+// gRPC
+
+type TokenizeServiceServer struct {
+	client pb.CountServiceClient
+}
+
+func NewTokenizeServiceServer(ctx context.Context, countSvcAddr string) (*TokenizeServiceServer, error) {
+	countSvcConn, err := grpc.DialContext(ctx, countSvcAddr,
+		grpc.WithInsecure(),
+		grpc.WithTimeout(grpcTimeout),
+		grpc.WithStatsHandler(&ocgrpc.ClientHandler{}))
+	if err != nil {
+		return nil, err
+	}
+	client := pb.NewCountServiceClient(countSvcConn)
+	return &TokenizeServiceServer{
+		client: client,
+	}, nil
+}
+
+func (t *TokenizeServiceServer) Tokenize(ctx context.Context, tr *pb.TokenizeRequest) (*pb.ModalResponse, error) {
+	span := trace.FromContext(ctx)
+	sc := span.SpanContext()
+	l := logger.With(
+		traceLogFieldKey, sc.TraceID.String(),
+		spanLogFieldKey, sc.SpanID.String(),
+	)
+
+	l.Infof("[tokenize] start tokenize")
+
+	// NOTE: very simple
+	src := tr.GetSourceText()
+	words := strings.Split(src, " ")
+	c := &pb.CountRequest{
+		Words: words,
+	}
+	mr, err := t.client.CountWords(ctx, c)
+	if err != nil {
+		l.Errorf("[tokenize] error tokenizing the words: %v", src)
+		return nil, err
+	}
+
+	l.Infof("[tokenize] end tokenize")
+
+	return mr, nil
+}
+
+// initialization
+
+func initLogger() (*zap.SugaredLogger, error) {
+	cfg := zap.Config{
+		Encoding:         "json",
+		Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:     "message",
+			LevelKey:       "severity",
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			TimeKey:        "timestamp",
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			CallerKey:      "caller",
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+	}
+
+	l, err := cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+	return l.Sugar(), nil
+}
+
+func initTraceAndStats() {
+	// init Trace with OpenCensus
+	// NOTE: trace.AlwaysSample() is for demo purpose
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	var err error
+	exporter, err = stackdriver.NewExporter(stackdriver.Options{
+		ProjectID:    projectID,
+		MetricPrefix: "tokenier",
+	})
+	if err != nil {
+		log.Fatalf("failed to create OC exporter: %v", err)
+	}
+	defer exporter.Flush()
+	trace.RegisterExporter(exporter)
+
+	// init Stats with OpenCensus
+	view.SetReportingPeriod(60 * time.Second)
+	view.RegisterExporter(exporter)
+	if err := view.Register(ocgrpc.DefaultServerViews...); err != nil {
+		log.Fatalf("failed to register server views: %v", err)
+	}
+	if err := view.Register(ocgrpc.DefaultClientViews...); err != nil {
+		log.Fatalf("failed to register client views: %v", err)
+	}
+}
